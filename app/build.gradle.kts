f892e3d4f2013e73890d517f90745e5b742720aa
HackDroid CI <actions@github.com>
2026-06-03 00:17:09 +0000

    bump: v1.0.10 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.industrialble.app"
         minSdk = 29
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.0.9"
+        versionCode = 10
+        versionName = "1.0.10"
 
         vectorDrawables {
             useSupportLibrary = true
